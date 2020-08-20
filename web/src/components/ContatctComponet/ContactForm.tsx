04ad4ca12a41e144b74117306d00d90bde8450e2
Leonardo Rebouças Batista <leo292629@gmail.com>
2020-08-19 21:19:26 -0300

    fixed message on cantact page

--- web/src/components/ContatctComponet/ContactForm.tsx
+++ web/src/components/ContatctComponet/ContactForm.tsx
@@ -1,64 +1,101 @@
-import React from 'react'
-import { Field, reduxForm, InjectedFormProps } from 'redux-form'
-import _ from 'lodash'
+import React from 'react';
+import { Field, reduxForm, InjectedFormProps } from 'redux-form';
+import _ from 'lodash';
 
-import { InputField } from '../InputField'
+import { RenderField } from '../RenderField';
 // import { validateEmail } from '../../utils/validateEmail'
-import { Email } from '../../actions'
+import { Email } from '../../actions';
 
 const formFields = [
-    {
-        label: 'Nome',
-        name: 'name',
-        type: 'text',
-        // noValueError: 'Digite seu nome',
-    },
-    {
-        label: 'Email',
-        name: 'from',
-        type: 'email',
-        // noValueError: 'Digite o email',
-    },
-    {
-        label: 'Assunto',
-        name: 'subject',
-        type: 'text',
-        // noValueError: 'Digite o assunto',
-    }
-]
+  {
+    label: 'Nome',
+    name: 'name',
+    type: 'text',
+    field: 'input',
+    className: 'inputField',
+    // noValueError: 'Digite seu nome',
+  },
+  {
+    label: 'Email',
+    name: 'from',
+    type: 'email',
+    field: 'input',
+    className: 'inputField',
+    // noValueError: 'Digite o email',
+  },
+  {
+    label: 'Assunto',
+    name: 'subject',
+    type: 'text',
+    field: 'input',
+    className: 'inputField',
+    // noValueError: 'Digite o assunto',
+  },
+  {
+    label: 'Mensagem',
+    name: 'text',
+    field: 'textarea',
+    className: 'mensagem',
+    // noValueError: 'Digite o assunto',
+  },
+];
 
 type fieldsType = {
-    name: string;
-    label: string;
-    type: string
-}
-
+  name: string;
+  label: string;
+  type: string;
+  field: string;
+  className: string;
+};
 
 interface ContactFormProps {
-    onSendEmail(email: Email): void
+  onSendEmail(email: Email): void;
 }
 
-class _ContactForm extends React.Component<ContactFormProps & InjectedFormProps<Email, ContactFormProps>> {
+class _ContactForm extends React.Component<
+  ContactFormProps & InjectedFormProps<Email, ContactFormProps>
+> {
+  render() {
+    const { pristine, submitting, handleSubmit, onSendEmail } = this.props;
+    return (
+      <div className="contact-form noselect">
+        <h5>Mande uma mensagem</h5>
+        <form
+          onSubmit={handleSubmit((formValues) => {
+            onSendEmail(formValues);
+          })}
+          autoComplete="off"
+          className="form-contact"
+        >
+          {_.map(
+            formFields,
+            ({ name, label, type, field, className }: fieldsType) => {
+              return (
+                <Field
+                  key={name}
+                  component={RenderField}
+                  type={type}
+                  label={label}
+                  name={name}
+                  field={field}
+                  className={className}
+                />
+              );
+            }
+          )}
+          {/* <textarea className="mensagem"></textarea> */}
 
-    render() {
-        const { pristine, submitting, handleSubmit, onSendEmail } = this.props
-        return (
-            <div className="contact-form noselect">
-                <h5>Mande uma mensagem</h5>
-                <form onSubmit={handleSubmit((formValues) => { onSendEmail(formValues) })} autoComplete="off" className="form-contact">
-
-                    {_.map(formFields, ({ name, label, type }: fieldsType) => {
-                        return <Field key={name} component={InputField} type={type} label={label} name={name} />
-                    })}
-                    <textarea className="mensagem">
-
-                    </textarea>
-
-                    <button className="noselect" disabled={pristine || submitting} type="submit">Enviar</button>
-                </form>
-            </div>
-        )
-    }
+          <button
+            className="noselect"
+            disabled={pristine || submitting}
+            type="submit"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 // const validate = (values: any) => {
@@ -71,4 +108,6 @@ class _ContactForm extends React.Component<ContactFormProps & InjectedFormProps<
 //     return errors
 // }
 
-export const ContactForm = reduxForm<Email, ContactFormProps>({ form: 'contacForm' })(_ContactForm)
+export const ContactForm = reduxForm<Email, ContactFormProps>({
+  form: 'contactForm',
+})(_ContactForm);
